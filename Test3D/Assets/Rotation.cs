0a7941b62ebd9999b0409ba10a210bb99cfe9b60
napat  srichantapong <tot14883@gmail.com>
2017-05-10 00:01:35 +0700

    Model3D
    พื้นฐานModel 3D ขอโอกาสการเข้าครั้งนี้

---
+++ Test3D/Assets/Rotation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour {
+    public float speed = 5;
+    private int i = 1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0) && i > 0)
+        {
+            Left();
+        }
+        if(Input.GetMouseButton(0)&& i < 0)
+        {
+            Right();
+        }
+	}
+    public void Left() {
+        i = 1;
+        transform.Rotate(0, -speed, 0);
+    }
+    public void Right()
+    {
+        i = -1;
+        transform.Rotate(0, +speed, 0);
+    }
+}
